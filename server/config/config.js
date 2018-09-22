0a0ea227d0c5786065167b27a2f9c5e685185966
werd2000 <walter.enrique.ruiz.diaz@gmail.com>
2018-09-21 23:26:33 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 //  Puerto
 // =====================================
 process.env.PORT = process.env.PORT || 3000;
+
+// =====================================
+//  Entorno
+// =====================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// =====================================
+//  Base de datos
+// =====================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+    // urlDB = 'mongodb://cafe-user:werd2000@ds211083.mlab.com:11083/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:werd2000@ds211083.mlab.com:11083/cafe';
+}
+process.env.URLDB = urlDB;
---
+++ server/routes/usuario.js
@@ -0,0 +1,140 @@
+const express = require('express');
+
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
+const Usuario = require('../models/usuario');
+
+const app = express();
+
+app.get('/usuario', function(req, res) {
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+    // Usuario.find({ acá van las condiciones de búsqueda }, 'los campos que quiero mostrar')
+    Usuario.find({ estado: true }, 'nombre email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err: err
+                });
+            }
+
+            Usuario.count({ estado: true }, (err, cantidad) => {
+                return res.json({
+                    ok: true,
+                    usuarios,
+                    cantidad
+                });
+            });
+
+        });
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: err
+            });
+        }
+
+        // usuarioDB.password = null;
+
+        return res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioBD) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        return res.json({
+            ok: true,
+            usuario: usuarioBD
+        });
+
+    });
+});
+
+app.delete('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+    let cambiaEstado = {
+        estado: false
+    };
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        return res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+    });
+});
+
+
+
+// =========================================================
+// Borrado físico de la BD
+// =========================================================
+// app.delete('/usuario/:id', function(req, res) {
+
+//     let id = req.params.id;
+//     Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+//         if (err) {
+//             return res.status(400).json({
+//                 ok: false,
+//                 err
+//             });
+//         }
+
+//         if (!usuarioBorrado) {
+//             return res.status(400).json({
+//                 ok: false,
+//                 err: {
+//                     message: 'Usuario no encontrado'
+//                 }
+//             });
+//         }
+
+//         return res.json({
+//             ok: true,
+//             usuario: usuarioBorrado
+//         });
+//     });
+// });
+
+module.exports = app;
